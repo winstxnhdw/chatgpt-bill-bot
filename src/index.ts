1bfb04285371d50c413c0c06c7b51a214eb222b9
winstxnhdw <winstxnhdw@gmail.com>
2023-06-03 03:44:24 +0800

    feat: add poll and dynamically count group members

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,24 @@ import { bankers_rounding, convert_money } from '@/utils'
 
 async function main() {
   const bot_instance = bot.build()
+  const member_count = await bot_instance.api.getChatMemberCount(config.TELEGRAM_CHAT_ID)
   const bill = await convert_money(20, 'USD', 'SGD')
-  const bill_per_pax = bankers_rounding(bill / 4, 2)
+  const bill_per_pax = bankers_rounding(bill / (member_count - 1), 2)
 
   await bot_instance.api.sendMessage(
     config.TELEGRAM_CHAT_ID,
     `Please pay Winston@${config.PHONE_NUMBER}\nAmount: SG$${bill_per_pax}\n\nThe price is calculated based on the current exchange rate.`
+  )
+
+  await bot_instance.api.sendPoll(
+    config.TELEGRAM_CHAT_ID,
+    'Have you paid?',
+    ['Paid 🤩', 'I am a stupid brokie who has not paid 😭'],
+    {
+      is_anonymous: false,
+      type: 'quiz',
+      correct_option_id: 0
+    }
   )
 }
 
