779369ef46184335efba3d383ba6286b53081bdf
winstxnhdw <winstxnhdw@gmail.com>
2023-08-29 03:42:49 +0800

    fix: remove unnecessary type assertion

--- src/utils/convert_money.ts
+++ src/utils/convert_money.ts
@@ -14,7 +14,7 @@ type Currency = keyof Rates
 
 const get_all_exchange_rates = async (): Promise<ExchangeRatesRequest> => {
   const request = await fetch(`http://data.fixer.io/api/latest?access_key=${config.FIXER_API_KEY}`)
-  return request.json() as Promise<ExchangeRatesRequest>
+  return request.json()
 }
 
 export const convert_money = async (amount: number, from: Currency, to: Currency): Promise<number> => {
