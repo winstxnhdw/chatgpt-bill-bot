a67d5ea07f94e46b866d0004c9ef2dc1985398bc
winstxnhdw <winstxnhdw@gmail.com>
2023-09-17 00:18:27 +0800

    refactor: use `num` validator for `TELEGRAM_CHAT_ID`

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-import { cleanEnv, makeValidator, str } from 'envalid'
+import { cleanEnv, makeValidator, num, str } from 'envalid'
 
 export const token = makeValidator((token) => {
   if (typeof token !== 'string') throw new Error('Token must be a string.')
@@ -8,7 +8,7 @@ export const token = makeValidator((token) => {
 
 export const config = cleanEnv(Bun.env, {
   TELEGRAM_BOT_TOKEN: token(),
-  TELEGRAM_CHAT_ID: str(),
+  TELEGRAM_CHAT_ID: num(),
   FIXER_API_KEY: str(),
   PHONE_NUMBER: str()
 })
