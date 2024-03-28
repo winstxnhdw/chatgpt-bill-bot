708950a8675281fc0b6e43f0484de1ed920558e9
winstxnhdw <winstxnhdw@gmail.com>
2024-03-28 23:58:08 +0800

    chore: remove `botInfo`

--- src/bot.ts
+++ src/bot.ts
@@ -1,15 +1,4 @@
 import { config } from '@/config'
 import { Bot } from 'grammy'
-import type { UserFromGetMe } from 'grammy/types'
 
-export const bot = new Bot(config.TELEGRAM_BOT_TOKEN, {
-  botInfo: {
-    is_bot: true,
-    first_name: 'ChatGPTBillBot',
-    username: 'ChatGPTBillBot',
-    language_code: 'en',
-    can_join_groups: true,
-    can_read_all_group_messages: true,
-    supports_inline_queries: true
-  } as UserFromGetMe
-})
+export const bot = new Bot(config.TELEGRAM_BOT_TOKEN)
